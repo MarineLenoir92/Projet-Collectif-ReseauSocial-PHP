28df5c8f015a38df024995149e926092eff6135e
Marine <marine.lenoir@hotmail.fr>
2022-09-20 15:06:29 +0200

    Fonctionnalité Session, Itération Déconnexion: bouton déconnexion + renvoi vers page login.php OK

--- logout.php
+++ logout.php
@@ -1,24 +1,5 @@
 <?php
 session_start();
 session_destroy();
+header("location: login.php");
 ?>
-
-<!doctype html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <title>ReSoC - Flux</title>         
-        <meta name="author" content="Julien Falconnet">
-        <link rel="stylesheet" href="style.css"/>
-    </head>
-    <body>
-    <?php include("header.php"); ?>
-    <div id="wrapper">
-        <main>
-            <article>
-                <h2>Vous avez été déconncté</h2>
-            </article>
-        </main>
-    </div>
-    </body>
-</html>
